1f87be9b5c9af4f5a75902060f4ea26cc025e469
Omar <147827001+omarshw@users.noreply.github.com>
2026-05-12 15:13:45 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+float CalculateAverage(float* grades, int subjects) {
+    float total = 0;
+    
+    for (int i = 0; i < subjects; i++) {
+        total += *(grades + i);
+    }
+    
+    return total / subjects;
+}
+
+float CalculateGPA(float average) {
+    return (average / 100.0) * 4.0;
+}
+
+void DisplayGrades(float* grades, int size) {
+    cout << "\nStudent Grades:\n";
+
+    for (int i = 0; i < size; i++) {
+
+        cout << "Subject "
+             << i + 1
+             << ": "
+             << *(grades + i)
+             << endl;
+    }
+}
+
+int main() {
+    string StudentID;
+
+    int NumberOfSubjects;
+
+    cout << "Enter Student ID: ";
+    cin >> StudentID;
+
+    cout << "Enter Number of Subjects: ";
+    cin >> NumberOfSubjects;
+
+    float grades[100];
+
+    for (int i = 0; i < NumberOfSubjects; i++) {
+        cout << "Enter Grade for Subject "
+             << i + 1
+             << ": ";
+
+        cin >> grades[i];
+    }
+
+    float average = CalculateAverage(grades, NumberOfSubjects);
+    float gpa = CalculateGPA(average);
+
+    cout << "\n========== STUDENT REPORT ==========\n";
+
+    cout << "Student ID: " << StudentID << endl;
+    if (gpa > 2)
+        cout << "Status: Passing" << endl;
+    else
+        cout << "Status: Failing" << endl;
+
+    DisplayGrades(grades, NumberOfSubjects);
+
+    cout << "\nAverage Grade: " << average << endl;
+    cout << "GPA: " << gpa << endl;
+
+
+    cout << "====================================\n";
+
+    return 0;
+}
